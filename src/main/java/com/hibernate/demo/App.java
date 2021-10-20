5f5f28b1194e642bab0f0a78b2c4ae155399734d
chandu Ujjina <chanduujjina129@gmail.com>
2021-10-20 11:01:42 +0530

    Hibernate Day2 demo Session methods

--- src/main/java/com/hibernate/demo/App.java
+++ src/main/java/com/hibernate/demo/App.java
@@ -1,9 +1,13 @@
 package com.hibernate.demo;
 
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.hibernate.demo.entity.Employee;
 import com.hibernate.demo.util.HibernateUtility;
@@ -17,18 +21,86 @@ public class App
     public static void main( String[] args )
     {
     	saveEmployee(new Employee("divya", "female"));
+    //Employee emp =	findEmployeeById(2);
+   // System.out.println(emp);
+    	//updateEmployeeById(new Employee("gouse patan", "male"),2);
+    	//deleteEmployeeById(3);
+    	
+    	// List<Employee> employeeList = getAllEmployees();
+    	// System.out.println(employeeList);
+    	 List<Employee> employeeList = findEmployeeByAttribute_v1("chandu","female");
+    	 System.out.println(employeeList);
     }
     
+    
+    
+    
    public static void  saveEmployee(Employee emp){
-	   SessionFactory factory = HibernateUtility.getSessionFactory();
 	   
-	   Session session =factory.openSession();
-	   
+	 Session session =  HibernateUtility.getSession();
 	  Transaction  transaction =session.beginTransaction();
-	   
 	   session.save(emp);
 	   transaction.commit();
 	   session.close();
 	   System.out.println(emp);
     }
+   
+   public static void updateEmployeeById(Employee emp,int empId) {
+	   Session session =  HibernateUtility.getSession();
+	   Transaction  transaction =session.beginTransaction();
+	 Employee employee =  (Employee) session.load(Employee.class, empId);
+	 
+	 employee.setName(emp.getName());
+	 employee.setGender(emp.getGender());
+	 session.saveOrUpdate(employee);
+	 transaction.commit();
+	 session.close();
+   }
+   
+   public static void deleteEmployeeById(int empId) {
+	   Session session =  HibernateUtility.getSession();
+	   Transaction  transaction =session.beginTransaction();
+	 Employee employee =  (Employee) session.load(Employee.class, empId);
+	 session.delete(employee);
+	 transaction.commit();
+	 session.close();
+   }
+   
+   public static List<Employee> getAllEmployees(){
+	   Session session =  HibernateUtility.getSession();
+	   Query query = session.createQuery("from Employee");
+	   List<Employee> employeeList = query.list();
+	   return employeeList;
+   }
+   
+   public static List<Employee> findEmployeeByAttribute(String name, String gender){
+	   Session session =  HibernateUtility.getSession();
+	 Criteria crit =  session.createCriteria(Employee.class);
+	 crit.add(Restrictions.eq("name", name));
+	 crit.add(Restrictions.eq("gender", gender));
+	 List<Employee> employeeList = crit.list();
+	   return employeeList;
+   }
+   
+   public static List<Employee> findEmployeeByAttribute_v1(String name, String gender){
+	   Session session =  HibernateUtility.getSession();
+	 Criteria crit =  session.createCriteria(Employee.class);
+	 crit.add(Restrictions.or(Restrictions.eq("name", name), Restrictions.eq("gender", gender)));
+	 List<Employee> employeeList = crit.list();
+	   return employeeList;
+   }
+   
+   
+   
+   public static Employee findEmployeeById(int empId) {
+	   Session session =  HibernateUtility.getSession();
+	    Employee emp = (Employee) session.load(Employee.class, empId);
+	    return emp;
+   }
+   
+   public static Employee findEmployeeById_v1(int empId) {
+	   Session session =  HibernateUtility.getSession();
+	    Employee emp = (Employee) session.get(Employee.class, empId);
+	    return emp;
+   }
 }
--- src/main/java/com/hibernate/demo/util/HibernateUtility.java
+++ src/main/java/com/hibernate/demo/util/HibernateUtility.java
@@ -1,5 +1,6 @@
 package com.hibernate.demo.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -11,6 +12,12 @@ public class HibernateUtility {
 		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
 		SessionFactory factory = configuration.buildSessionFactory(builder.build());
 		return factory;
+	}
+	
+	public static Session getSession() {
+		SessionFactory sessionFactory= getSessionFactory();
+		 Session session = sessionFactory.openSession();
+		 return session;
 	}
 
 }
